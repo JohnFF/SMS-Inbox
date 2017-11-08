8dcb901d2a4b4187e10938b5d128dcc3d78d8dcf
John <john@civifirst.com>
2017-11-08 00:45:44 +0000

    Added SMS Inbox Data

--- CRM/Smsinbox/Page/SmsInbox.php
+++ CRM/Smsinbox/Page/SmsInbox.php
@@ -9,19 +9,34 @@ class CRM_Smsinbox_Page_SmsInbox extends CRM_Core_Page {
       'options' => array('limit' => 0),
     ));
 
-    foreach ($inboundSmsMessages as &$eachInboundSmsMessage) {
-      $eachInboundSmsMessage['to'] = civicrm_api3('contact', 'getvalue', array(
+    foreach ($inboundSmsMessages['values'] as &$eachInboundSmsMessage) {
+
+      $eachInboundSmsMessage['from'] = 'test';
+      $eachInboundSmsMessage['to'] = 'test';
+
+      if (empty($eachInboundSmsMessage['source_contact_id'])) {
+        continue;
+      }
+
+      $eachInboundSmsMessage['from'] = civicrm_api3('contact', 'getvalue', array(
         'return' => 'display_name',
         'id' => $eachInboundSmsMessage['source_contact_id'],
       ));
 
-      $eachInboundSmsMessage['from'] = civicrm_api3('contact', 'get', array(
-        'return' => 'display_name',
+      $fromContactId = civicrm_api3('activity_contact', 'getvalue', array(
+        'return' => 'contact_id',
         'activity_id' => $eachInboundSmsMessage['id'],
+        'record_type_id' => 3,
+      ));
+
+
+      $eachInboundSmsMessage['to'] =  civicrm_api3('contact', 'getvalue', array(
+          'return' => 'display_name',
+          'id' => $fromContactId,
       ));
     }
 
-    $this->assign('inboundSmsMessages', $inboundSmsMessages);
+    $this->assign('inboundSmsMessages', $inboundSmsMessages['values']);
 
     parent::run();
   }
